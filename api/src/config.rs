a8cddfb31966fcd9756622ef822bbf209614df85
akagiyuu <khanghuynh0245@gmail.com>
2025-05-13 13:23:39 +0700

    feat(config): add jwt configs

--- api/src/config.rs
+++ api/src/config.rs
@@ -10,6 +10,14 @@ fn default_cors_domain() -> String {
     "http://localhost:3000".to_string()
 }
 
+fn default_jwt_secret() -> String {
+    "secret".to_string()
+}
+
+const fn default_jwt_expired_in() -> u64 {
+    26 * 60 * 60
+}
+
 #[derive(Deserialize)]
 pub struct Config {
     #[serde(default = "default_port")]
@@ -17,6 +25,12 @@ pub struct Config {
 
     #[serde(default = "default_cors_domain")]
     pub cors_domain: String,
+
+    #[serde(default = "default_jwt_secret")]
+    pub jwt_secret: String,
+
+    #[serde(default = "default_jwt_expired_in")]
+    pub jwt_expired_in: u64,
 }
 
 pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {

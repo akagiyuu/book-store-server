a7df3be50f143adf2c8bf458fcbd3bd0041ce182
akagiyuu <khanghuynh0245@gmail.com>
2025-05-14 21:52:20 +0700

    refactor: simplify structure

--- crates/database/src/queries.rs
+++
@@ -1,7 +0,0 @@
-// This file was generated with `clorinde`. Do not modify.
-
-pub mod author;
-pub mod book;
-pub mod category;
-pub mod review;
-pub mod user;
--- queries/user.sql
+++
@@ -1,6 +0,0 @@
---! insert
-INSERT INTO users(email, password, first_name, last_name)
-VALUES (:email, :password, :first_name, :last_name);
-
---! get_by_email
-SELECT id, password FROM users WHERE email = :email LIMIT 1;

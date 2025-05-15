cd4de1def4417f99bec14b0ef9a08fdb40de30d7
akagiyuu <khanghuynh0245@gmail.com>
2025-05-15 08:55:16 +0700

    fix(google-auth): use oauth2 version 4.4.2

--- src/state.rs
+++ src/state.rs
@@ -1,30 +1,12 @@
-use oauth2::{
-    AuthUrl, ClientId, ClientSecret, EmptyExtraTokenFields, EndpointNotSet, EndpointSet,
-    RedirectUrl, RevocationErrorResponseType, StandardErrorResponse, StandardRevocableToken,
-    StandardTokenIntrospectionResponse, StandardTokenResponse, TokenUrl,
-    basic::{BasicClient, BasicErrorResponseType, BasicTokenType},
-};
+use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl, basic::BasicClient};
 use sqlx::PgPool;
 
 use crate::{Result, config::CONFIG};
 
-type GoogleOAuthClient = oauth2::Client<
-    StandardErrorResponse<BasicErrorResponseType>,
-    StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>,
-    StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>,
-    StandardRevocableToken,
-    StandardErrorResponse<RevocationErrorResponseType>,
-    EndpointSet,
-    EndpointNotSet,
-    EndpointNotSet,
-    EndpointNotSet,
-    EndpointSet,
->;
-
 #[derive(Debug)]
 pub struct ApiState {
     pub database: PgPool,
-    pub google_oauth_client: GoogleOAuthClient,
+    pub google_oauth_client: BasicClient,
     pub http_client: reqwest::Client,
 }
 
@@ -32,17 +14,13 @@ impl ApiState {
     pub async fn new() -> Result<Self> {
         let database = PgPool::connect(&CONFIG.database_url).await.unwrap();
 
-        let google_oauth_client = BasicClient::new(ClientId::new(CONFIG.google_client_id.clone()))
-            .set_client_secret(ClientSecret::new(CONFIG.google_client_secret.clone()))
-            .set_auth_uri(
-                AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string()).unwrap(),
-            )
-            .set_token_uri(
-                TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string()).unwrap(),
-            )
-            .set_redirect_uri(
-                RedirectUrl::new(CONFIG.google_authorized_redirect_url.clone()).unwrap(),
-            );
+        let google_oauth_client = BasicClient::new(
+            ClientId::new(CONFIG.google_client_id.clone()),
+            Some(ClientSecret::new(CONFIG.google_client_secret.clone())),
+            AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string()).unwrap(),
+            Some(TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string()).unwrap()),
+        )
+        .set_redirect_uri(RedirectUrl::new(CONFIG.google_authorized_redirect_url.clone()).unwrap());
 
         let http_client = reqwest::ClientBuilder::new()
             .redirect(reqwest::redirect::Policy::none())
